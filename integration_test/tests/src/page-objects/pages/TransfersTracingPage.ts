65bdde04b5caed26f14020c21efb29c4e46f78b1
Kevin <kleyow@gmail.com>
2021-11-29 20:26:21 -0500

    chore: more tests

--- integration_test/tests/src/page-objects/pages/TransfersTracingPage.ts
+++ integration_test/tests/src/page-objects/pages/TransfersTracingPage.ts
@@ -1,14 +1,24 @@
-import { Selector } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
 export type TransferRow = {
   row: Selector,
   id: Selector,
+  transferState: Selector,
+  type: Selector,
+  currency: Selector,
+  amount: Selector,
+  payerDFSPId: Selector,
+  payeeDFSPId: Selector,
+  settlementBatchId: Selector,
+  dateSubmitted: Selector,
 }
 
 export const FindTransfersPage = {
-  transferId: Selector('.rc-field').withAttribute('placeholder', 'Transfer ID' ),
-  clearFiltersButton: Selector('.rc-button').withText('Clear Filters' ),
-  findTransfersButton: Selector('.rc-button').withText('Find Transfers' ),
+  transferIdSearchField: Selector('.placeholder').withText('Transfer ID').sibling('.rc-textfield'),
+  currencyFilterField: Selector('.placeholder').withText('Currency').sibling('.rc-textfield'),
+  transferStateFilterField: Selector('.placeholder').withText('Transfer State').sibling('.rc-select'),
+  clearFiltersButton: Selector('.rc-button').withText('Clear Filters'),
+  findTransfersButton: Selector('.rc-button').withText('Find Transfers'),
 
   async getResultRows(): Promise<TransferRow[]> {
     const rows = Selector('.rc-table__body__row');
@@ -18,8 +28,19 @@ export const FindTransfersPage = {
       .map((_, i) => ({
         row: rows.nth(i),
         id: rows.nth(i).find('.rc-table__body__cell').nth(0),
+        transferState: rows.nth(i).find('.rc-table__body__cell').nth(1),
+        type: rows.nth(i).find('.rc-table__body__cell').nth(2),
+        currency: rows.nth(i).find('.rc-table__body__cell').nth(3),
+        amount: rows.nth(i).find('.rc-table__body__cell').nth(4),
+        payerDFSPId: rows.nth(i).find('.rc-table__body__cell').nth(5),
+        payeeDFSPId: rows.nth(i).find('.rc-table__body__cell').nth(6),
+        settlementBatchId: rows.nth(i).find('.rc-table__body__cell').nth(7),
+        dateSubmitted: rows.nth(i).find('.rc-table__body__cell').nth(8),
       }));
   },
 
-  getTransferDetailsModal: (transferId: string) => Selector('.rc-modal').withProps({ title: `Transfer ${transferId} Details` }),
+  getTransferDetailsModal: (transferId: string) => Selector('.rc-modal__header__title')
+    .withText(`Transfer ${transferId} Details`)
+    .parent().parent('.rc-modal'),
+  selectTransferStateFilter: (transferState: string) => t.click(Selector('.rc-select__option__label').withText(transferState).parent()).wait(1000),
 };
--- integration_test/tests/src/tests/FindTransfersPage.test.ts
+++ integration_test/tests/src/tests/FindTransfersPage.test.ts
@@ -14,21 +14,30 @@ fixture `Find Transfers Feature`
     const hubAccounts: protocol.HubAccount[] = [
       {
         type: "HUB_MULTILATERAL_SETTLEMENT",
-        currency: "MMK",
+        currency: "USD",
       },
       {
         type: "HUB_RECONCILIATION",
-        currency: "MMK",
+        currency: "USD",
+      },
+      {
+        type: "HUB_MULTILATERAL_SETTLEMENT",
+        currency: "EUR",
+      },
+      {
+        type: "HUB_RECONCILIATION",
+        currency: "EUR",
       },
     ];
     await cli.createHubAccounts(hubAccounts);
     ctx.cli = cli;
 
     const accounts: protocol.AccountInitialization[] = [
-      { currency: 'MMK', initial_position: '0', ndc: 10000 },
-      { currency: 'MMK', initial_position: '0', ndc: 10000 },
+      { currency: 'USD', initial_position: '0', ndc: 10000 },
+      { currency: 'USD', initial_position: '0', ndc: 10000 },
+      { currency: 'EUR', initial_position: '0', ndc: 10000 },
+      { currency: 'EUR', initial_position: '0', ndc: 10000 },
     ];
-
 
     const participants = await cli.createParticipants(accounts);
     ctx.participants = participants;
@@ -39,7 +48,7 @@ fixture `Find Transfers Feature`
     const transfers1: protocol.TransferMessage[] = [{
       msg_sender: participants[1].name,
       msg_recipient: participants[0].name,
-      currency: 'MMK',
+      currency: 'USD',
       amount: '10',
       transfer_id: uuidv4(),
     }];
@@ -49,30 +58,50 @@ fixture `Find Transfers Feature`
     const transfers2: protocol.TransferMessage[] = [{
       msg_sender: participants[0].name,
       msg_recipient: participants[1].name,
-      currency: 'MMK',
+      currency: 'USD',
       amount: '10',
       transfer_id: uuidv4(),
     }];
     ctx.transfers.push(transfers2[0]);
     await cli.completeTransfers(transfers2);
+
+    const transfers3: protocol.TransferMessage[] = [{
+      msg_sender: participants[2].name,
+      msg_recipient: participants[3].name,
+      currency: 'EUR',
+      amount: '10',
+      transfer_id: uuidv4(),
+    }];
+    ctx.transfers.push(transfers3[0]);
+    await cli.completeTransfers(transfers3);
+
+    // This transfer will fail and become 'ABORTED' due to invalid currency
+    const transfers4: protocol.TransferMessage[] = [{
+      msg_sender: participants[2].name,
+      msg_recipient: participants[3].name,
+      currency: 'AUD',
+      amount: '10',
+      transfer_id: uuidv4(),
+    }];
+    ctx.transfers.push(transfers4[0]);
+    await cli.completeTransfers(transfers4);
   })
   .beforeEach(async (t) => {
     await waitForReact();
     await t
-      .click(SideMenu.transfersButton).wait(5000);
+      .click(SideMenu.transfersButton).wait(1000);
   });
 
 test.meta({
   ID: '',
   STORY: '',
-  description:
-    `Find transfers with no filter selected should return transfers`,
-})('Find transfers with no filter selected should return transfers', async (t) => {
+  description: '',
+})('Find transfers with no filters selected should return transfers', async (t) => {
   // navigate to the find transfers page
-  await t.click(SideMenu.transfersButton).wait(5000);
+  await t.click(SideMenu.transfersButton).wait(1000);
 
   // click the find transfers button (no filters selected by default)
-  await t.click(FindTransfersPage.findTransfersButton).wait(5000);
+  await t.click(FindTransfersPage.findTransfersButton).wait(1000);
 
   // we should see two or more rows, one for each transfer we executed above
   const rows = await FindTransfersPage.getResultRows();
@@ -83,7 +112,192 @@ test.meta({
   const rowIds = await Promise.all(rows.map((r: TransferRow) => r.id.innerText));
 
   for(let i = 0; i < transferIds.length; i++) {
-    await t.expect(rowIds).contains(transferIds[i], 'rows contains the transfer');
+    await t.expect(rowIds).contains(transferIds[i], 'rows dont contain transfers');
   }
 });
 
+test.meta({
+  ID: '',
+  STORY: '',
+  description: '',
+})('Clicking on transfer row should Transfer Modal popup', async (t) => {
+  // navigate to the find transfers page
+  await t.click(SideMenu.transfersButton).wait(1000);
+
+  // click the find transfers button (no filters selected by default)
+  await t.click(FindTransfersPage.findTransfersButton);
+
+  // get all rows found
+  const rows = await FindTransfersPage.getResultRows();
+
+  // click the first found row
+  await t.click(rows[0].row);
+  const transferId = await rows[0].id.innerText;
+
+  const popup = FindTransfersPage.getTransferDetailsModal(transferId);
+
+  await t.expect(popup.exists).ok('Transfer details popup not found in dom');
+  await t.click(popup.find('.rc-tabs__tab').withText('Basic Information')).wait(1000);
+  t.expect(popup.find('.rc-tabs__tab--selected').withText('Basic Information'));
+});
+
+test.meta({
+  ID: '',
+  STORY: '',
+})('Navigate to Technical Details Tab of Transfer Modal popup',
+  async (t) => {
+    // navigate to the find transfers page
+    await t.click(SideMenu.transfersButton).wait(1000);
+
+    // click the find transfers button (no filters selected by default)
+    await t.click(FindTransfersPage.findTransfersButton);
+
+    // get all rows found
+    const rows = await FindTransfersPage.getResultRows();
+
+    // click the first found row
+    await t.click(rows[0].row);
+    const transferId = await rows[0].id.innerText;
+
+    const popup = FindTransfersPage.getTransferDetailsModal(transferId);
+    await t.expect(popup.exists).ok('Transfer details popup not found in dom');
+    await t.click(popup.find('.rc-tabs__tab').withText('Technical Details')).wait(1000);
+    t.expect(popup.find('.rc-tabs__tab--selected').withText('Technical Details'));
+  },
+);
+
+test.meta({
+  ID: '',
+  STORY: '',
+  description: '',
+})('Find transfer by specifying transfer ID', async (t) => {
+  // navigate to the find transfers page
+  await t.click(SideMenu.transfersButton).wait(1000);
+
+  // Enter transfer id into search field
+  await t.typeText(FindTransfersPage.transferIdSearchField, t.fixtureCtx.transfers[0].transfer_id).wait(1000);
+
+  // click the find transfers button (no filters selected by default)
+  await t.click(FindTransfersPage.findTransfersButton).wait(1000);
+
+  // we should see exactly one transfer
+  const rows = await FindTransfersPage.getResultRows();
+  await t.expect(rows.length).eql(1);
+
+  const rowIds = await Promise.all(rows.map((r: TransferRow) => r.id.innerText));
+  await t.expect(rowIds[0]).eql(t.fixtureCtx.transfers[0].transfer_id, 'rows dont contain transfer');
+});
+
+test.meta({
+  ID: '',
+  STORY: '',
+  description: '',
+})('Filter transfers by currency', async (t) => {
+  // navigate to the find transfers page
+  await t.click(SideMenu.transfersButton).wait(1000);
+
+  // Enter currency to filter by
+  await t.typeText(FindTransfersPage.currencyFilterField, 'EUR').wait(1000);
+
+  // click the find transfers button (no filters selected by default)
+  await t.click(FindTransfersPage.findTransfersButton).wait(1000);
+
+  const rows = await FindTransfersPage.getResultRows();
+  const rowCurrency = await Promise.all(rows.map((r: TransferRow) => r.currency.innerText));
+
+  for(let i = 0; i < rows.length; i++) {
+    await t.expect(rowCurrency[i]).contains('EUR', 'rows dont contain transfers');
+  }
+});
+
+
+test.meta({
+  ID: '',
+  STORY: '',
+  description: '',
+})('Filter transfers by state', async (t) => {
+  // navigate to the find transfers page
+  await t.click(SideMenu.transfersButton).wait(1000);
+
+  // Enter transfer state to filter by
+  await t.typeText(FindTransfersPage.transferStateFilterField, 'ABORTED').wait(1000);
+  await FindTransfersPage.selectTransferStateFilter('ABORTED');
+
+  // click the find transfers button (no filters selected by default)
+  await t.click(FindTransfersPage.findTransfersButton).wait(1000);
+
+  const rows = await FindTransfersPage.getResultRows();
+  const rowState = await Promise.all(rows.map((r: TransferRow) => r.transferState.innerText));
+
+  for(let i = 0; i < rows.length; i++) {
+    await t.expect(rowState[i]).contains('ABORTED', 'rows dont contain transfers');
+  }
+});
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Filter Transfers by Payer FSPID, Payer Id and Payer value',
+  async (t) => {
+    // To be able to perform filters on Payer/Payee we need to
+    // perform quotes with the participants so that they populate the
+    // `quoteParty` table. This will involve having to rework the harness
+    // to use the testing toolkit.
+  },
+);
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Filter Transfers by Payee FSPID, Payee Id and Payee value',
+  async (t) => {
+    // To be able to perform filters on Payer/Payee we need to
+    // perform quotes with the participants so that they populate the
+    // `quoteParty` table. This will involve having to rework the harness
+    // to use the testing toolkit.
+  },
+);
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Navigate to Party Events Modal of Transfer Modal',
+  async (t) => {
+    // This requires upgrading the test harness to initiate party lookup
+    // requests with the ALS. Will also require using the TTK and adding the
+    // account-lookup-service sidecar.
+  },
+);
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Navigate to Quote Events Modal of Transfer Modal',
+  async (t) => {
+    // This requires upgrading the test harness to initiate quote requests
+    // requests with the ALS. Will also require using the TTK and adding the
+    // quoting-service sidecar.
+  },
+);
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Navigate to Transfer Events Modal of Transfer Modal',
+  async (t) => {
+    // This requires upgrading the test harness to initiate party lookup
+    // requests with the ALS. This requires the adding of the
+    // central-ledger-handler-* sidecars.
+  },
+);
+
+test.skip.meta({
+  ID: '',
+  STORY: '',
+})('Navigate to Settlement Events Modal of Transfer Modal',
+  async (t) => {
+    // This requires upgrading the test harness to initiate settlement
+    // requests with the central-settlements service.
+    // This requires the adding of the central-settlement sidecar.
+  },
+);
