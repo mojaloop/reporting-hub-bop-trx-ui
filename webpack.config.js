8cc7542437ac47e3067d8d8ca3f7804fc9f7ec32
Kalin Krustev <kalin.krustev@gmail.com>
2025-09-11 07:26:02 +0000

    fix: update webpack config to support different devtool settings based on mode

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const config = {
 
 const { ModuleFederationPlugin } = webpack.container;
 
-module.exports = {
+module.exports = (env, argv) => ({
   // Class names are needed for integration testing of the production build
   // `testcafe-react-selector` needs these classnames to be present
   optimization: {
@@ -36,7 +36,7 @@ module.exports = {
     ],
   },
   entry: './src/index',
-  devtool: 'cheap-module-source-map',
+  devtool: argv.mode === 'production' ? 'source-map' : 'eval-cheap-module-source-map',
   devServer: {
     disableHostCheck: true,
     // Enable gzip compression of generated files.
@@ -179,4 +179,4 @@ module.exports = {
       template: './public/index.html',
     }),
   ].filter(Boolean),
-};
+});
